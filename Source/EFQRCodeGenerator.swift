10056979c3def0e173c101ff4b80ae061f438b31
AnkurLahiry <alahiry098@gmail.com>
2021-07-11 13:59:31 +0600

    Fix missing file.

--- Source/EFQRCodeGenerator.swift
+++ Source/EFQRCodeGenerator.swift
@@ -1402,24 +1402,3 @@ public class EFQRCodeGenerator: NSObject {
         return nil
     }
 }
-
-public enum EFQRCodePosition {
-    case topLeftOuter
-    case topLeftInner
-    case topRightOuter
-    case topRightInner
-    case bottomLeftOuter
-    case bottomLeftInner
-    case unknown
-}
-
-public struct EFQRCustomization {
-    let color: UIColor
-    let position: EFQRCodePosition
-    let shape: EFPointShape = .square
-    
-    public init(color: UIColor, position: EFQRCodePosition) {
-        self.color = color
-        self.position = position
-    }
-}
